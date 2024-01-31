57e17bb1564d919ec87670ab85d7e8c9429d27b7
GuilhermeFrSaito <guilhermefsaito@gmail.com>
2024-01-31 16:48:58 -0300

    Correction to MainPageInitial commit of Basic Implementation of WeatherForecast App

---
+++ WeatherForecast/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace WeatherForecast
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ WeatherForecast/Pages/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace WeatherForecast.Pages
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage(Location? location)
+        {
+            InitializeComponent();
+
+            if (location != null)
+            {
+                var responseFromApi = Task.Run(() => GetWeatherResponse()).Result;
+                
+                Console.WriteLine(responseFromApi);
+            }
+        }
+
+        private static async Task<string> GetWeatherResponse()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://weatherapi-com.p.rapidapi.com/current.json?q=-22.0154%2C-47.9207"),
+                Headers =
+                {
+                    { "X-RapidAPI-Key", " " },
+                    { "X-RapidAPI-Host", "weatherapi-com.p.rapidapi.com" },
+                },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                return body;
+            }
+        }
+        
+    }
+
+}
